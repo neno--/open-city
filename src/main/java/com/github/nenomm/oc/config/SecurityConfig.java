fdac00902f082352260cb2eb3060a5c0db432cc3
nenad <nenad.nenadovic@kapsch.net>
2018-05-06 10:55:55 +0200

    add security config for auth and non auth routes

--- src/main/java/com/github/nenomm/oc/config/SecurityConfig.java
+++ src/main/java/com/github/nenomm/oc/config/SecurityConfig.java
@@ -6,11 +6,12 @@ import com.github.nenomm.oc.security.CustomExceptionHandlerFilter;
 import com.github.nenomm.oc.security.CustomTokenAuthenticationFilter;
 import com.github.nenomm.oc.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -25,19 +26,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	private ExceptionTranslator exceptionTranslator;
 
 	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.authenticationProvider(customAuthenticationProvider);
-	}
-
-	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
 		http.authorizeRequests()
-				.antMatchers("/v1/cities/**").permitAll()
+				.antMatchers(HttpMethod.GET, "/v1/cities/**").permitAll()
 				.antMatchers("/v1/token").permitAll()
-				.antMatchers("/v1/users").permitAll();
+				.antMatchers("/v1/users").permitAll()
+				.antMatchers(HttpMethod.POST, "/v1/cities/**").authenticated()
+				.antMatchers("/v1/users/**/favorites").authenticated();
 
-		http.antMatcher("/v1/users/**/favorites").authorizeRequests().and()
+		http.requestMatcher(new AntPathRequestMatcher("/v1/users/cities/**", HttpMethod.POST.name()))
+				.addFilterBefore(new CustomTokenAuthenticationFilter(userService), BasicAuthenticationFilter.class)
+				.addFilterBefore(new CustomExceptionHandlerFilter(exceptionTranslator), CustomTokenAuthenticationFilter.class);
+
+		http.antMatcher("/v1/users/**/favorites")
 				.addFilterBefore(new CustomTokenAuthenticationFilter(userService), BasicAuthenticationFilter.class)
 				.addFilterBefore(new CustomExceptionHandlerFilter(exceptionTranslator), CustomTokenAuthenticationFilter.class);
 	}
