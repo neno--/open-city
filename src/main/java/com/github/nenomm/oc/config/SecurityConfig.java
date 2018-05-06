413a721d7dade5d71eda75f004b5886b4efdb978
nenad <nenad.nenadovic@kapsch.net>
2018-05-06 10:33:21 +0200

    add minimal working spring conf

--- src/main/java/com/github/nenomm/oc/config/SecurityConfig.java
+++ src/main/java/com/github/nenomm/oc/config/SecurityConfig.java
@@ -6,18 +6,13 @@ import com.github.nenomm.oc.security.CustomExceptionHandlerFilter;
 import com.github.nenomm.oc.security.CustomTokenAuthenticationFilter;
 import com.github.nenomm.oc.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
-@Configuration
-@EnableWebMvcSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true)
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
@@ -37,18 +32,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
-		http.exceptionHandling().and().csrf().disable().headers().frameOptions().disable().and()
-				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
 		http.authorizeRequests()
 				.antMatchers("/v1/cities/**").permitAll()
 				.antMatchers("/v1/token").permitAll()
 				.antMatchers("/v1/users").permitAll();
 
-		http.antMatcher("/v1/users/**/favorites").authorizeRequests()
-				.anyRequest().authenticated().and()
+		http.antMatcher("/v1/users/**/favorites").authorizeRequests().and()
 				.addFilterBefore(new CustomTokenAuthenticationFilter(userService), BasicAuthenticationFilter.class)
-				.addFilterBefore(new CustomExceptionHandlerFilter(exceptionTranslator), CustomTokenAuthenticationFilter.class)
-				.authorizeRequests();
+				.addFilterBefore(new CustomExceptionHandlerFilter(exceptionTranslator), CustomTokenAuthenticationFilter.class);
 	}
 }
